ec35516bda8079633e01c896846cde1cc5e792c2
Rodrigo <rodrigo.o.2012.r@gmail.com>
2024-01-29 00:18:22 -0300

    Desafio entregable 2
    En esta carpeta se encuentra el desafio entregable + un hands on labs que estaba probando

---
+++ desafio03/ProductManager.js
@@ -0,0 +1,119 @@
+import crypto from 'crypto';
+const producto1 = {
+    title: "Jabon",
+    description: null,
+    price: 123,
+    thumbnail: "Sin imagen",
+    code: "asd123",
+    stock: 20
+}
+const producto2 = {
+    title: "Lavarropas",
+    description: "Lava lava",
+    price: 4562,
+    code: "e2e4s",
+    stock: 2
+}
+const producto3 = {
+    title: "Pendrive",
+    description: "Pendrive con formita de lego",
+    price: 523,
+    thumbnail: "Sin imagen",
+    code: "qwe123",
+    stock: 30
+}
+const producto4 = {
+    title: "Escritorio",
+    description: "Escritorio de melamina 90cm",
+    price: 2456,
+    thumbnail: "Sin imagen",
+    code: "4rt4f4",
+    stock: 10
+}
+
+class ProductManager {
+
+    #modeloObj = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+
+    constructor(path) {
+        this.products = [];
+        this.path = path;
+    }
+
+    addProduct(producto) {
+        let existencia = this.products.some(item => item.code === producto.code);
+        if (existencia) {
+            console.log("El producto ya existe, no se puede agregar")
+            return "El producto ya existe"
+        } else {
+            let propiedadProducto = true, valorProducto = true;
+
+            this.#modeloObj.forEach((item) => {
+                if (!Object.keys(producto).includes(item)) {
+                    console.log("No se ha ingresado una o mas claves")
+                    propiedadProducto = false;
+                    return null;
+                }
+            })
+            Object.values(producto).forEach(item => {
+                if (item === '' || item === null) {
+                    console.log("No se aceptan campos vacíos o nulos")
+                    valorProducto = false;
+                    return
+                }
+            })
+            if (propiedadProducto && valorProducto) {
+                producto.id = crypto.randomBytes(8).toString('hex');
+                this.products.push(producto)
+            }
+        }
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(Id) {
+        const resultado = this.products.find(item => item.id === Id);
+        if (resultado) {
+            return resultado
+        } else {
+            console.log("Not Found")
+        }
+    }
+
+    updateProduct(id, dato) {
+        const productoIndex = this.products.findIndex(item => item.id === id);
+        for (let item in dato) {
+            if (Object.keys(this.products[productoIndex]).includes(item)) {
+                this.products[productoIndex][item] = dato[item];
+            }
+        }
+        console.log("Campos actualizados")
+        console.table(this.products[productoIndex])
+    }
+
+    deleteProduct(id) {
+        const productoIndex = this.products.findIndex(item => item.id === id);
+        this.products.splice(productoIndex, 1)
+        console.log("Producto eliminado");
+    }
+}
+
+
+let manejadorProductos = new ProductManager();
+console.log(manejadorProductos.getProducts());
+manejadorProductos.addProduct(producto3)
+console.table(manejadorProductos.getProducts())
+manejadorProductos.addProduct(producto3)
+manejadorProductos.addProduct(producto4)
+
+let myIdProducto = manejadorProductos.getProducts()[0].id;
+console.log(manejadorProductos.getProductById(myIdProducto))
+manejadorProductos.getProductById("asd8asd2")
+
+//Entrega nro 2. Clase 4
+manejadorProductos.updateProduct(myIdProducto, { title: "Mouse", stock: 34 })
+console.table(manejadorProductos.getProducts())
+manejadorProductos.deleteProduct(myIdProducto);
+console.table(manejadorProductos.getProducts())
