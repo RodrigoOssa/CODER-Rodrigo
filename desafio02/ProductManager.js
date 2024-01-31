6a405c0961b462bdbcc07b6a02040cec33f8a9a2
Rodrigo <rodrigo.o.2012.r@gmail.com>
2024-01-31 16:17:42 -0300

    Carga desafío 2
    Corrección del desafío usando promesas.
    + orden de carpeta con los opcionales

--- desafio02/ProductManager.js
+++ desafio02/ProductManager.js
@@ -1,119 +1,163 @@
 import crypto from 'crypto';
-const producto1 = {
-    title: "Jabon",
-    description: null,
-    price: 123,
-    thumbnail: "Sin imagen",
-    code: "asd123",
-    stock: 20
-}
-const producto2 = {
-    title: "Lavarropas",
-    description: "Lava lava",
-    price: 4562,
-    code: "e2e4s",
-    stock: 2
-}
-const producto3 = {
-    title: "Pendrive",
-    description: "Pendrive con formita de lego",
-    price: 523,
-    thumbnail: "Sin imagen",
-    code: "qwe123",
-    stock: 30
-}
-const producto4 = {
-    title: "Escritorio",
-    description: "Escritorio de melamina 90cm",
-    price: 2456,
-    thumbnail: "Sin imagen",
-    code: "4rt4f4",
-    stock: 10
-}
+import fs from 'fs';
 
 class ProductManager {
 
+    //Modelo de como debería de llegar el objeto
     #modeloObj = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
 
     constructor(path) {
         this.products = [];
         this.path = path;
+        //Corroborar la existencia del json
+        !fs.existsSync(this.path) ?
+            fs.writeFileSync(this.path, "[]") :
+            null
     }
 
-    addProduct(producto) {
-        let existencia = this.products.some(item => item.code === producto.code);
-        if (existencia) {
+    /**
+    * Lee archivo JSON de productos
+    *
+    * @return Productos parseados
+    */
+    async getProducts() {
+        const productos = await fs.promises.readFile(this.path, 'utf-8')
+            .then(data => data)
+            .catch(err => err)
+        return JSON.parse(productos)
+    }
+
+    /**
+    * Agrega un producto nuevo si este no existe.
+    *
+    * Retorna false si existe.
+    * 
+    * Retorna true si no existe y se agregó correctamente.
+    * @param {obj} producto 
+    * @return {boolean}
+    */
+    async addProduct(producto) {
+        //Corrobora que producto esté en el JSON
+        let productosJSON = await this.getProducts()
+            .then(products =>
+                products.some(item => item.code === producto.code))
+            .catch(() => console.log("Hubo un error al obtener products"))
+            .catch(err => err)
+        if (productosJSON) {
             console.log("El producto ya existe, no se puede agregar")
-            return "El producto ya existe"
+            return false
         } else {
             let propiedadProducto = true, valorProducto = true;
-
+            //Se valida que el objeto tenga las llaves correspondientes
             this.#modeloObj.forEach((item) => {
                 if (!Object.keys(producto).includes(item)) {
                     console.log("No se ha ingresado una o mas claves")
                     propiedadProducto = false;
-                    return null;
+                    return false
                 }
             })
+            //Se valida que el objeto no tenga valores vacios o nulos
             Object.values(producto).forEach(item => {
                 if (item === '' || item === null) {
                     console.log("No se aceptan campos vacíos o nulos")
                     valorProducto = false;
-                    return
+                    return false
                 }
             })
             if (propiedadProducto && valorProducto) {
+                //Se crea un id y se agrega al JSON
                 producto.id = crypto.randomBytes(8).toString('hex');
-                this.products.push(producto)
+                const newProductosJson = await this.getProducts();
+                newProductosJson.push(producto);
+                return await fs.promises.writeFile(this.path, JSON.stringify(newProductosJson))
+                    .then(() => console.log("Producto agregado."))
+                    .then(() => true)
+                    .catch(err => console.log(err))
+                    .catch(() => false)
+
             }
         }
     }
 
-    getProducts() {
-        return this.products;
-    }
-
-    getProductById(Id) {
-        const resultado = this.products.find(item => item.id === Id);
-        if (resultado) {
-            return resultado
+    /**
+    * Busca un producto mediante el Id
+    * @param {string} Id producto a buscar
+    * 
+    */
+    async getProductById(Id) {
+        const resultado = await this.getProducts()
+            .then(productos => productos.find(item => item.id === Id))
+            .catch(err => err)
+        if (!resultado) {
+            console.log("No se encontró el producto")
+            return null
         } else {
-            console.log("Not Found")
+            return resultado
         }
     }
 
-    updateProduct(id, dato) {
-        const productoIndex = this.products.findIndex(item => item.id === id);
-        for (let item in dato) {
-            if (Object.keys(this.products[productoIndex]).includes(item)) {
-                this.products[productoIndex][item] = dato[item];
+    /**
+    * Busca un producto mediante el Id.
+    *
+    * Retorna true si pudo actualizar los datos.
+    * 
+    * Retorna false si no pudo actualizar los datos.
+    * 
+    * @param {string} id Id producto a buscar.
+    * @param {obj} dato Obj nuevo con los datos a actualizar.
+    * @return {boolean} 
+    */
+    async updateProduct(id, dato) {
+        const productoIndex = await this.getProducts()
+            .then(productIndex => productIndex.findIndex(item => item.id === id))
+        let newProductosJson = await this.getProducts()
+            .then(products => products)
+        if (productoIndex !== -1) {
+            for (let item in dato) {
+                if (Object.keys(newProductosJson[productoIndex]).includes(item)) {
+                    newProductosJson[productoIndex][item] = dato[item];
+                }
             }
+            return await fs.promises.writeFile(this.path, JSON.stringify(newProductosJson))
+                .then(() => console.log("Producto actualizado."))
+                .then(() => true)
+                .catch(err => console.log(err))
+                .catch(() => false)
+        } else {
+            console.log("No se encontró el producto");
+            return false
         }
-        console.log("Campos actualizados")
-        console.table(this.products[productoIndex])
     }
 
-    deleteProduct(id) {
-        const productoIndex = this.products.findIndex(item => item.id === id);
-        this.products.splice(productoIndex, 1)
-        console.log("Producto eliminado");
+    /**
+    * Elimina un producto mediante id
+    *
+    * Retorna true si pudo eliminar el producto.
+    * 
+    * Retorna false si no pudo eliminar el producto.
+    * 
+    * @param {string} id Id producto a eliminar.
+    *
+    * @return {boolean} 
+    */
+    async deleteProduct(id) {
+        const productoIndex = await this.getProducts()
+            .then(productIndex => productIndex.findIndex(item => item.id === id))
+        if (productoIndex !== -1) {
+            let newProductosJson = await this.getProducts()
+                .then(products => products)
+            newProductosJson.splice(productoIndex, 1)
+            return await fs.promises.writeFile(this.path, JSON.stringify(newProductosJson))
+                .then(() => console.log("Producto eliminado"))
+                .then(() => true)
+                .catch(err => console.log(err))
+                .catch(() => false)
+        } else {
+            console.log("El producto no existe")
+            return false
+        }
     }
 }
 
 
-let manejadorProductos = new ProductManager();
-console.log(manejadorProductos.getProducts());
-manejadorProductos.addProduct(producto3)
-console.table(manejadorProductos.getProducts())
-manejadorProductos.addProduct(producto3)
-manejadorProductos.addProduct(producto4)
-
-let myIdProducto = manejadorProductos.getProducts()[0].id;
-console.log(manejadorProductos.getProductById(myIdProducto))
-manejadorProductos.getProductById("asd8asd2")
-
-//Entrega nro 2. Clase 4
-manejadorProductos.updateProduct(myIdProducto, { title: "Mouse", stock: 34 })
-console.table(manejadorProductos.getProducts())
-manejadorProductos.deleteProduct(myIdProducto);
-console.table(manejadorProductos.getProducts())
+export default ProductManager;
