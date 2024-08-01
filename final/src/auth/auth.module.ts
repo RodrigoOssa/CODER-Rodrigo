29318a867cfa62a17b5ae37323b53005daddc7f7
Rodrigo <rodrigo.o.2012.r@gmail.com>
2024-07-31 23:12:12 -0300

    Configuracion de JWT terminada. Funcionando. Falta implementar la autenticacion en los endpoints

--- final/src/auth/auth.module.ts
+++ final/src/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { DatabaseModule } from 'src/database/database.module';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY'),
+        signOptions: { expiresIn: '300s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService]
 })
 export class AuthModule { }
--- final/src/auth/auth.service.ts
+++ final/src/auth/auth.service.ts
@@ -1,19 +1,33 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
 import { User } from 'src/users/schemas/user.schema';
+import { AccessTokenDTO } from './dto/accessToken.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
-        @Inject('USER_MODEL') private userModel: Model<User>
+        @Inject('USER_MODEL') private userModel: Model<User>,
+        private readonly jwtService: JwtService
     ) { }
 
-    async singIn(userEmail: string, password: string) {
+    async singIn(userEmail: string, password: string): Promise<AccessTokenDTO> {
         const user = await this.userModel.findOne({ email: userEmail });
         if (user?.password === password) {
-            //Acá hay que generar y mandar el token
-            console.log(user)
-            return user
+            const { _id, first_name, last_name, age, email, role } = user;
+            const userData = {
+                _id,
+                first_name,
+                last_name,
+                age,
+                email,
+                role
+            }
+            const token = {
+                access_token: await this.jwtService.signAsync(userData)
+            };
+            console.log(token)
+            return token
         } else {
             console.log(user)
             throw new UnauthorizedException();
